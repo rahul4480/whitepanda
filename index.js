9c088aa5650e2b04ea8c41a826be955f2378bcab
Rahul <rahul.chhabra1998@gmail.com>
2019-10-05 21:11:43 +0530

    whitepanda commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var comments = document.getElementById("mybutton3");
+
+function myFunction(x){
+	x.style.width = "130px";
+	comments.style.right = "230px";
+	x.classList.toggle("fas fa-code");
+}
+
+function myFnctn(x){
+	x.style.width = "40px";
+	comments.style.right = "120px";
+	x.classList.toggle("fas fa-code");
+}
